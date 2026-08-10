7d92c09f21f45d659f92b83b3d3b1137bbc508ee
saracen75 <saracen75@gmail.com>
2026-08-11 03:16:57 +0800

    removed name as required field for collaterals in WrappedToken.sol

--- src/WrappedToken.sol
+++ src/WrappedToken.sol
@@ -7,6 +7,11 @@ pragma solidity ^0.8.36;
  *          created 08Aug2026
  *          modified 10Aug2026
  *              added `deDupeCollateralList()` function that produces a new Collateral[] list with unique addresses and names.
+ *          modified 11Aug2026
+ *              removed `name` field for tokens.
+ *              removed `Collateral` struct and replaced all instances with `address`.
+ *              removed `deDupeCollateralList()` function.
+ *              updated `_acceptedCollateralsList` to `_acceptedCollaterals`.
  * @notice  Abstract contract implementing basic functionality of a wrapped token:
  *          1. receives collateral tokens and returns wrapped tokens.
  *          2. receives wrapped tokens and returns collateral tokens.
@@ -27,36 +32,31 @@ pragma solidity ^0.8.36;
  *          - must we mess around with price oracles too?
  */
 abstract contract WrappedToken {
-    struct Collateral {
-        address addr;
-        string name;
-    }
-
     /////////////////////////////////////////////////////////////////////////////////////////
     // state variables
     /////////////////////////////////////////////////////////////////////////////////////////
     /**
-     * @notice  _acceptedCollateralsList
+     * @notice  _acceptedCollaterals
      *          state variable
-     *          List of accepted collateral token addresses and their utf8 readable names.
+     *          List of accepted collateral token addresses.
      * @dev     Stored as array to be enumerable.
+     * @dev     Ths addresses in this list are unique.
      * @dev     List is intended to be short (< 30 items) to maintain access efficiency and avoid out-of-gas reverts in function calls.
      *          Problems begin at ~400 items. Functions accessing this list break at ~1000 items due to out of gas.
      *          However as an abstract contract designed to be extended, the upper limit is left to the implementer to specify.
      *          The upper limit may be enforced by overriding the `_exceedSizeLimit()` function.
      */
-    Collateral[] private _acceptedCollateralsList;
-    mapping(address collateral => string Name) private _acceptedCollateralsMap;
+    address[] private _acceptedCollaterals;
     mapping(address user => mapping(address asset => uint256 balance)) private _balances;
 
     /////////////////////////////////////////////////////////////////////////////////////////
     // constructor
     /////////////////////////////////////////////////////////////////////////////////////////
-    constructor(Collateral[] memory initialAcceptedCollaterals) {
+    constructor(address[] memory initialAcceptedCollaterals) {
         uint256 len = initialAcceptedCollaterals.length;
         if (len > 0 && !_exceedSizeLimit(len)) {
             for (uint256 i = 0; i < len; i++) {
-                _addAcceptedCollateral(initialAcceptedCollaterals[i].addr, initialAcceptedCollaterals[i].name);
+                _addAcceptedCollateral(initialAcceptedCollaterals[i]);
             }
         }
     }
@@ -64,10 +64,10 @@ abstract contract WrappedToken {
     /////////////////////////////////////////////////////////////////////////////////////////
     // external/public functions
     /////////////////////////////////////////////////////////////////////////////////////////
-    function addAcceptedCollaterals(Collateral[] calldata addList) external virtual returns (bool) {}
-    function removeAcceptedCollaterals(Collateral[] calldata removeList) external virtual returns (bool) {}
-    function getAcceptedCollateralList() external view virtual returns (Collateral[] memory list) {
-        return _acceptedCollateralsList;
+    function addAcceptedCollaterals(address[] calldata addList) external virtual returns (bool) {}
+    function removeAcceptedCollaterals(address[] calldata removeList) external virtual returns (bool) {}
+    function getAcceptedCollateralList() external view virtual returns (address[] memory) {
+        return _acceptedCollaterals;
     }
     function isAccepted(address token) external view virtual returns (bool) {
         return _isAccepted(token);
@@ -82,14 +82,12 @@ abstract contract WrappedToken {
      * @notice  _addAcceptedCollateral function
      *          Adds a token to the accepted collaterals list.
      */
-    function _addAcceptedCollateral(address token, string memory name) internal virtual returns (bool) {
+    function _addAcceptedCollateral(address token) internal virtual returns (bool) {
         // safety checks
-        if (bytes(name).length == 0) {return false;}
         if (_isAccepted(token)) {return false;}
-        if (_exceedSizeLimit(_acceptedCollateralsList.length + 1)) {return false;}
+        if (_exceedSizeLimit(_acceptedCollaterals.length + 1)) {return false;}
         // add the collateral to accepted list and mapping
-        _acceptedCollateralsList.push(Collateral({addr: token, name:name}));
-        _acceptedCollateralsMap[token] = name;
+        _acceptedCollaterals.push(token);
         return true;
     }
 
@@ -101,7 +99,9 @@ abstract contract WrappedToken {
         // sanity checks
         if (token == address(0)) {return false;}
         if (token == address(this)) {return false;}
-        return bytes(_acceptedCollateralsMap[token]).length > 0;
+        uint256 len = _acceptedCollaterals.length;
+        for (uint256 i = 0; i < len; i++) {if (_acceptedCollaterals[i] == token) {return true;}}
+        return false;
     }
 
     /**
@@ -113,58 +113,7 @@ abstract contract WrappedToken {
      *          appropriate custom errors to revert when exceeded.
      */
     function _exceedSizeLimit(uint256 len) internal pure returns (bool) {
-        if (len > 30) revert("max size for _acceptedCollateralsList exceeded");
+        if (len > 30) revert("max size for _acceptedCollaterals exceeded");
         return false;
     }
-
-    /**
-     * @notice  deDupeCollateralList function
-     *          De-duplicates a Collateral[] list and returns a new Collateral[] list with no duplicates and resized appropriately.
-     * @dev     Enforced de-duplication rules:
-     *          - all tokens in the accepted collaterals list must have valid and unique addresses and names.
-     *          - invalid addresses: zero address (0x0), this contract (address(this))
-     *          - invalid names: empty-string names.
-     */
-    function deDupeCollateralList(Collateral[] memory list) internal pure returns (Collateral[] memory deDuped) {
-        // if list has < 2 items, return as is
-        uint256 len = list.length;
-        if (len < 2) {return list;}
-
-        deDuped = new Collateral[](len);
-        bytes32[] memory deDupedHashes = new bytes32[](len);
-        uint256 deDupedCount = 0;
-
-        for (uint256 i = 0;i < len;) {
-            address addr = list[i].addr;
-            bytes memory name = bytes(list[i].name);
-            bytes32 nameHash = keccak256(name);
-            uint256 nameLen = name.length;
-            if (nameLen > 0) {
-                uint256 j = 0;
-                for (; j < deDupedCount;) {
-                    if ((addr == deDuped[j].addr) ||
-                        ((nameLen == bytes(deDuped[j].name).length) && (nameHash == deDupedHashes[j]))) {break;}
-                    unchecked {j++;}
-                }
-                if (j == deDupedCount) {
-                    deDuped[deDupedCount] = list[i];
-                    deDupedHashes[deDupedCount] = nameHash;
-                    unchecked {deDupedCount++;}
-                }
-            }
-            unchecked {i++;}
-        }
-        assembly {mstore(deDuped, deDupedCount)}
-    }
-
-
-    /* not implementing batched transactions for now.
-    struct TokenAmount {
-        address addr;
-        uint256 amount;
-    }
-    function isAccepted(address[] memory collaterals) external view returns (bool) {}
-    function deposit(TokenAmount[] memory deposits) external returns (uint256 wrappedAmount) {}
-    function burn(TokenAmount[] memory withdrawals, uint256 burnAmount) external returns (TokenAmount[] memory successfulWithdrawals) {}
-    */
 }
